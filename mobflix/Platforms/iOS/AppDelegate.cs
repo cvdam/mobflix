b63c919c763928bb1a484563d4ae99a21ee87a08
cvdam <cassio.donato@outlook.com>
2022-10-24 16:04:24 -0300

    Added logo image

---
+++ mobflix/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace mobflix;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
